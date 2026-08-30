395e48c240c2b596c1d7cdf10223774168d02882
shanks-t <shanksleslietrey@gmail.com>
2026-08-30 19:02:52 +0000

    build simple kernel from scrtach with torch inline

--- curriculum/gpu-mode-lecture-002/vector_addition/vec_add.cu
+++ curriculum/gpu-mode-lecture-002/vector_addition/vec_add.cu
@@ -1,5 +1,7 @@
 #include <cuda.h>
 #include <stdio.h>
+#include <c10/cuda/CUDAException.h>
+#include <c10/cuda/CUDAStream.h>
 
 
 __global__
@@ -17,11 +19,31 @@ inline unsigned int cdiv(unsigned int a, unsigned int b) {
 
 torch::Tensor vector_add(torch::Tensor a, torch::Tensor b) {
     // validate a and b are CUDA float tensors
+    assert(a.device().type() == torch::kCUDA);
+    assert(b.device().type() == torch::kCUDA);
+    assert(a.dtype() == torch::kFloat);
+    assert(b.dtype() == torch::kFloat);
+
     // validate they have equal length
+    assert(a.dim() == 1);
+    assert(b.dim() == 1);
+    assert(a.numel() == b.numel());
     // allocate output
+    auto output = torch::empty_like(a);
     // choose 256 threads per block
+    dim3 threads_per_block(256);
     // calculate number of blocks with cdiv
+    dim3 number_of_blocks(cdiv(a.numel(), threads_per_block.x));
     // launch vector_add_kernel
+    vector_add_kernel<<<number_of_blocks, threads_per_block, 0, torch::cuda::getCurrentCUDAStream()>>>(
+        output.data_ptr<float>(),
+        a.data_ptr<float>(),
+        b.data_ptr<float>(),
+        a.numel()
+    );
     // launch error check
+    C10_CUDA_KERNEL_LAUNCH_CHECK();
+
     // return output
+    return output;
 }
